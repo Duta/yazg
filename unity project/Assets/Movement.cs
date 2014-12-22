6c66b8b22822877a6bc22bf6353f5f6b6c5a8507
Bertie Wheen <wheen.b@gmail.com>
2014-12-22 23:43:40 +0000

    Unity stuff that will be deleted anyway

---
+++ unity project/Assets/Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public float movementSpeed = 10f;
+	public float rotationSpeed = 10f;
+	public CharacterController cc;
+	private Vector3 goal;
+
+	void Update() {
+		if(Input.GetMouseButton(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)) {
+				goal = hit.point;
+			}
+		}
+		if(Vector3.Distance(transform.position, goal) > 1) {
+			Quaternion rotation = Quaternion.LookRotation(goal - transform.position);
+			rotation.x = 0f;
+			rotation.z = 0f;
+			transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+			cc.SimpleMove(movementSpeed * transform.forward);
+		}
+	}
+}
---
+++ unity project/Assets/TrackPlayer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackPlayer : MonoBehaviour {
+	public GameObject player;
+	public Vector3 offset;
+
+	void Update () {
+		transform.position = player.transform.position + offset;
+	}
+}
